84479a966e0de8d09159c0d04910148252572b3f
dusan.veselinovic.dev gmail.com <dusan.veselinovic.dev@gmail.com>
2022-02-25 20:56:46 +0100

    Feat: Add result delay to be sync with sound

--- public/index.js
+++ public/index.js
@@ -8,12 +8,13 @@
  const Default = Object.freeze({
   Credit: 1000,
   Reels: 3,
-  Symbols: 12,
+  NumberOfSymbolsInReel: 11, // MAX 19 extend reelSymbols if you want more
   MinBet: 1,
   MaxBet: 20,
   musicVol: 0.4,
   gameVol: 0.1,
   autoSpinSpeed: 3000,
+  autoSpinNextSpinDelay: 2000,
   reelsSpinRound: 2,
   reelSymbols: [
     {
@@ -120,12 +121,13 @@
 });
 
 class OgreSlotMachine{
-  constructor(credit = Default.Credit, reels = Default.Reels, symbols = Default.Symbols){
+  constructor(credit = Default.Credit, reels = Default.Reels, symbols = Default.NumberOfSymbolsInReel){
       this.credit = credit;
       this.reels = reels;
       this.reelsBucket = [];
       this.betValue = Default.MinBet;
       this.autoSpinInterval;
+      this.autoSpinSpeed;
       this.populateReelsBuckets(reels);
   }
 
@@ -146,6 +148,7 @@ class OgreSlotMachine{
       let symbolCounts = {};
       let winSum = 0;
       let self = this;
+      let maxTimeout;
 
       if(document.getElementsByClassName('slot__reel').length){
         let t = document.querySelectorAll('.slot__reel');
@@ -189,16 +192,18 @@ class OgreSlotMachine{
       function displayReels(resultCombination, self) {
         resultCombination.forEach((reelResul) => {
           let addedReel = self.addReels(Default.reelSymbols);
-          self.stopReels(addedReel, Number(reelResul), Default.reelsSpinRound, Default.Symbols);
+          let timeout = self.stopReels(addedReel, Number(reelResul), Default.reelsSpinRound, Default.NumberOfSymbolsInReel);
+          maxTimeout = maxTimeout || timeout;
+          if(maxTimeout <= timeout){ maxTimeout = timeout}
         });
       }
+
       
       setBetValue(e, self);
       resultReels(this.reelsBucket, self);
       displayReels(resultCombination, self);
       winSum = winReels(countReels(resultCombination), self);
-      this.calculateCredit(winSum);
-      // this.draw(resDisplay);
+      setTimeout(() => this.calculateCredit(winSum), maxTimeout);
   }
 
   autoSpin(e) {
@@ -211,7 +216,7 @@ class OgreSlotMachine{
     } else {
       this.autoSpinInterval = setInterval(() => {
         this.spin(e);
-      }, Default.autoSpinSpeed)
+      }, Default.autoSpinSpeed + Default.autoSpinNextSpinDelay)
       btn.classList.add("btn--blinkBorder");
     }
 
@@ -230,8 +235,8 @@ class OgreSlotMachine{
     
     // REF: Avoid if statments
     if(value > 0) {
-      console.log('WIN' + value);
-      note.style.borderColor = '#38bb6f';
+      // console.log('WIN' + value);
+      // note.style.borderColor = '#38bb6f';
       audio = new Audio('./media/sounds/Multicast_x4.mp3.mp3');
       audio.volume = Default.gameVol;
       audio.play();
@@ -240,8 +245,8 @@ class OgreSlotMachine{
       // Display how much user earned
       paid.value = value;
     } else {
-      console.log('LOSE');
-      note.style.borderColor = '#ee6052';
+      // console.log('LOSE');
+      // note.style.borderColor = '#ee6052';
       audio = new Audio('./media/sounds/spin.mp3');
       audio.volume = Default.gameVol;
       audio.play();
@@ -301,7 +306,7 @@ class OgreSlotMachine{
     blockContainer.classList.add("reel__window");
     blockRailway.classList.add("reel__railway");
   
-    for(var i = 0; i <= Default.Symbols; i++){
+    for(var i = 0; i <= Default.NumberOfSymbolsInReel; i++){
       block = document.createElement("div");
       block.classList.add("reel__symbol");
       block.innerHTML = i;
@@ -309,7 +314,7 @@ class OgreSlotMachine{
       blockRailway.appendChild(block);
   
       // Make clone of last element, and put it at start
-      if(i == Default.Symbols){
+      if(i == Default.NumberOfSymbolsInReel){
         cache = document.createElement("div");
         cache.classList.add("reel__symbol");
         cache.innerHTML = i;
@@ -328,7 +333,7 @@ class OgreSlotMachine{
     let container1 = document.getElementsByClassName("reel__symbol");
     let blockMarginTop = parseInt(getComputedStyle(container1[0]).marginTop)
     let blockRealHeight = container1[0].clientHeight + blockMarginTop;
-    let blockTotalHeight = (Default.Symbols * blockRealHeight + (blockRealHeight));
+    let blockTotalHeight = (Default.NumberOfSymbolsInReel * blockRealHeight + (blockRealHeight));
     let intervalSpinReels;
     blockRailway.style.top = -blockTotalHeight + 'px';
   
@@ -362,8 +367,8 @@ class OgreSlotMachine{
     setTimeout(() => {
       clearInterval(reel.interval)
     }, stopTimer);
-  
-    console.log(((timeout+((numberOfSymbols-winnerReel) * reel.timeout)) + ((numberOfSymbols+1) * reel.timeout) * rounds));
+    return stopTimer;
+    // console.log(((timeout+((numberOfSymbols-winnerReel) * reel.timeout)) + ((numberOfSymbols+1) * reel.timeout) * rounds));
   }
   
   
